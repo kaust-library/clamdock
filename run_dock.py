1ee62af071f63c2feeece7c027107707dba2e433
Marcelo Garcia <marcelomgarcia@gmail.com>
2023-10-07 21:33:16 +0300

    Updated run of antivirus routine

--- run_dock.py
+++ run_dock.py
@@ -2,32 +2,56 @@
 # this is just a wrapper.
 
 
-import subprocess as sb
+import subprocess as sp
 import configparser as cp
 import pathlib as pl
+import datetime as dt
 import logging as log
 import sys
 
 
-def checkAV(av_config):
-    """
-    Run the antivirus part:
-    1. Run the first time and create a quarantine file
-    2. Check if the quarantine is over:
-    2.1 If quarantine is over, run the antivirus one more time.
-    2.2 If ingestion is still under quarantine, just exit.
-    """
+# def checkAV(av_config):
+#     """
+#     Run the antivirus part:
+#     1. Run the first time and create a quarantine file
+#     2. Check if the quarantine is over:
+#     2.1 If quarantine is over, run the antivirus one more time.
+#     2.2 If ingestion is still under quarantine, just exit.
+#     """
 
-    quarentine_dir = pl.Path(av_config["quarantine_dir"])
-    if not quarentine_dir.exists():
-        log.warning(f"Creating quarantine dir '{quarentine_dir}'")
-        quarentine_dir.mkdir()
+#     quarentine_dir = pl.Path(av_config["quarantine_dir"])
+#     if not quarentine_dir.exists():
+#         log.warning(f"Creating quarantine dir '{quarentine_dir}'")
+#         quarentine_dir.mkdir()
 
-    quarentine_file = (
-        pl.Path(pl.Path.cwd())
-        .joinpath(av_config["quarantine_dir"])
-        .joinpath(av_config["av_accession"])
-    )
+#     quarentine_file = (
+#         pl.Path(pl.Path.cwd())
+#         .joinpath(av_config["quarantine_dir"])
+#         .joinpath(av_config["av_accession"])
+#     )
+
+
+def runAV(av_config):
+    dt_run_av = dt.datetime.today().strftime("%Y%m%d")
+    log.info("Updating AV database")
+    av_update = """docker run -it --rm 
+    --name fresh_clam_db --mount source=clam_db,target=/var/lib/clamav 
+    clamav/clamav:latest freshclam"""
+
+    result = sp.run(av_update, stdout=p.PIPE, stderr=sp.STDOUT, text=True)
+    av_log_file = f"clamAVlog{av_config['av_accession']}_{dt_run_av}.txt"
+    docker_run = f"docker run -it --rm --name aa_docker"
+    docker_target = f"-v \"{av_config['av_location']}:/scandir\""
+    docker_log_target = f"-v \"{av_config['av_logs_root']}:/logs\""
+    clam_db = "--mount source=clam_db,target=/var/lib/clamav"
+    clam_run = "clamav/clamav:latest clamscan /scandir"
+    clam_options = " --recursive=yes --verbose"
+    log_av = f"--log=/logs/{av_log_file}"
+    av_check = f"{docker_run} {docker_target} {docker_log_target} {clam_db} {clam_run} {log_av} {clam_options}"
+    log.info(f"Antivirus check: {av_check}")
+    result = sp.run(av_check, stdout=sp.PIPE, stderr=sp.STDOUT)
+    result.check_returncode
+    av_log = result.stdout
 
 
 def main() -> None:
