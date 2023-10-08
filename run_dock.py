6a5d18dfd08722d07d109a11dce5650f5fc4a6f6
Marcelo Garcia <marcelomgarcia@gmail.com>
2023-10-08 16:21:50 +0300

    Copy files routine

--- run_dock.py
+++ run_dock.py
@@ -45,7 +45,7 @@ def runAV(av_config):
     #
     # Antivirus.
     av_log_file = f"clamAVlog{av_config['av_accession']}_{dt_run_av}.txt"
-    docker_run = f"docker run -it --rm --name aa_docker"
+    docker_run = f"docker run --rm --name aa_docker"
     docker_target = f"-v \"{av_config['av_location']}:/scandir\""
     docker_log_target = f"-v \"{av_config['av_logs_root']}:/logs\""
     clam_db = "--mount source=clam_db,target=/var/lib/clamav"
@@ -59,15 +59,37 @@ def runAV(av_config):
     av_log = result.stdout
 
 
-def main() -> None:
-    """
-    Run a docker container.
-    """
+def copyFiles(f_config):
+    src = [ff.strip() for ff in f_config["source_dir"].split(",")]
+    dest = f_config["dest_dir"]
 
+    #
+    # Docker container to copy files
+    docker_run = "docker run --rm --name copy_files"
+    docker_target = f"-v {dest}:/dest"
+    docker_image = "debian:bookworm-slim"
+    copy_cmd = "cp -pr /src /dest"
+
+    for ss in src:
+        log.info(f"Copying files from '{ss}' to '{dest}'")
+        docker_source = f"-v {ss}:/src"
+        copy_files = (
+            f"{docker_run} {docker_source} {docker_target} {docker_image} {copy_cmd}"
+        )
+        result = sp.run(copy_files, stdout=sp.PIPE, stderr=sp.STDOUT)
+        result.returncode
+
+
+def main() -> None:
+    #
+    # Logging
+    log.basicConfig(level=log.INFO, format="%(asctime)s %(message)s")
+    #
+    # Input file
     config_file = sys.argv[1]
 
-    # av_config = read_config(file_cfg, "CLAMAV")
-
+    #
+    # Set system configuration
     log.info(f"Reading configuration file '{config_file}'")
     config = cp.ConfigParser(interpolation=cp.ExtendedInterpolation())
     config.read(config_file)
@@ -78,9 +100,12 @@ def main() -> None:
 
     #
     # ClamAV
+    # runAV(config['CLAMAV'])
 
     #
     # Copy source files to destination folder
+    log.info("Copying files")
+    copyFiles(config["BAGGER"])
 
 
 if __name__ == "__main__":
