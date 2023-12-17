0cacbe9474efa85f4203be9776be58e61d32bd0b
Marcelo Garcia <marcelomgarcia@gmail.com>
2023-12-17 18:49:07 +0300

    Initial commit

---
+++ airflow/dags/update_av_db_dag.py
@@ -0,0 +1,16 @@
+from airflow import DAG
+from airflow.providers.docker.operators.docker import DockerOperator
+
+with DAG(
+    dag_id="update_db_dag",
+    description="Update AV database"
+) as dag:
+    update_db = DockerOperator(
+        task_id="update_db",
+        image="clamav/clamav",
+        command=[
+            "freshclam",
+        ],
+        mounts=(source="clam_db", target="/var/lib/clamav"),
+        network_mode="airflow",
+    )
