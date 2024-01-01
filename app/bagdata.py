efeaf72cdceaff183408bb234a659513fa9e2106
Marcelo Garcia <garcm0b@KAUST.EDU.SA>
2024-01-01 16:55:20 +0300

    Wrong call of environ

--- app/bagdata.py
+++ app/bagdata.py
@@ -4,19 +4,19 @@ from os import environ
 
 def main():
     BagIt_test = {
-        "Source-Organization": f"{environ['SOURCE_ORGANIZATION']}",
-        "External-Identifier": f"{environ['EXTERNAL_IDENTIFIER']}",
-        "Internal-Sender-Description": f"{environ['INTERNAL_SENDER_DESCRIPTION']}",
-        "Title": f"{environ['TITLE']}",
-        "Date-Start": f"{environ['DATE_START']}",
-        "Record-Creators": f"{environ['RECORD_CREATORS']}",
-        "Record-Type": f"{environ['RECORD_TYPE']}",
-        "Extend-Size": f"{environ['EXTEND_SIZE']}",
-        "Subjects": f"{environ['SUBJECTS']}",
-        "Office": f"{environ['OFFICE']}",
+        "Source-Organization": environ["SOURCE_ORGANIZATION"],
+        "External-Identifier": environ["EXTERNAL_IDENTIFIER"],
+        "Internal-Sender-Description": environ["INTERNAL_SENDER_DESCRIPTION"],
+        "Title": environ["TITLE"],
+        "Date-Start": environ["DATE_START"],
+        "Record-Creators": environ["RECORD_CREATORS"],
+        "Record-Type": environ["RECORD_TYPE"],
+        "Extend-Size": environ["EXTEND_SIZE"],
+        "Subjects": environ["SUBJECTS"],
+        "Office": environ["OFFICE"],
     }
 
-    bag_path = environ["BAG_PATH"]
+    bag_path = "/mydir"
 
     my_bag = make_bag(bag_path, BagIt_test, checksums=["sha256"])
 
